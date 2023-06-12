e39d0395230e1b6db290eb14078acfeb40d1c015
_notSalt <joshuakoh303@gmail.com>
2023-06-12 15:20:23 +0800

    some updates, need to verify snow

--- kubejs/server_scripts/conflicts.js
+++ kubejs/server_scripts/conflicts.js
@@ -1,7 +1,7 @@
 ServerEvents.tags('item', event => {
   event.add('blue_skies:planks', /blue_skies:.+?_planks/)
   event.add('byg:planks', /byg:.+?_planks/)
-  let chestsMissingTags = ['hexerei:willow_chest', 'hexerei:mahogany_chest', 'ars_nouveau:archwood_chest', 'ad_astra:strophar_chest', 'ad_astra:aeronos_chest']
+  let chestsMissingTags = ['hexerei:willow_chest', 'hexerei:mahogany_chest', 'ars_nouveau:archwood_chest']
   event.add('forge:chests', chestsMissingTags)
   event.add('forge:chests/wooden', chestsMissingTags)
 })
@@ -10,4 +10,45 @@ ServerEvents.recipes(event => {
   // cooked eggs recipe conflict
   event.remove({ output: "incubation:fried_egg" })
   event.remove({ output: "naturalist:cooked_egg" })
+  event.smoking("minecraft:egg", "farmersdelight:fried_egg")
+
+  // blue skies tools
+  let planks = Ingredient.of('#minecraft:planks').subtract(Ingredient.of('#blue_skies:planks'))
+  event.forEachRecipe({ id: /minecraft:wooden_(hoe|shovel|pickaxe|sword|axe)/ }, recipe => {
+    let json = recipe.json
+    let key = json.get('key')
+    key.add('X', planks.toJson())
+    json.add('key', key)
+    recipe.json = json
+  })
+  event.forEachRecipe({ id: 'minecraft:stick' }, recipe => {
+    let json = recipe.json
+    let key = json.get('key')
+    key.add('#', planks.toJson())
+    json.add('key', key)
+    recipe.json = json
+  })
+  let cobble = Ingredient.of('#quark:stone_tool_materials').subtract(Ingredient.of('#blue_skies:cobblestone'))
+  event.forEachRecipe({ id: /quark:tweaks\/crafting\/utility\/tools\/stone_(hoe|shovel|pickaxe|sword|axe)/ }, recipe => {
+    let json = recipe.json
+    let key = json.get('key')
+    key.add('X', cobble.toJson())
+    json.add('key', key)
+    recipe.json = json
+  })
+
+  // remove cake recipe, create does it better
+  event.remove({ id: 'minecraft:cake' })
+
+  // lots of crafting tables
+  let tablePlanks = planks.subtract(Ingredient.of('#byg:planks'))
+  event.forEachRecipe({ id: 'minecraft:crafting_table' }, recipe => {
+    let json = recipe.json
+    let key = json.get('key')
+    key.add('#', tablePlanks.toJson())
+    json.add('key', key)
+    recipe.json = json
+  })
+
+
 })
